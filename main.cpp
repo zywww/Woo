c74f5457dcf15543d56cdef077e099032f1d6f0d
TanSilver <coty0@sina.com>
2016-02-27 23:43:12 +0800

    some change

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@ using namespace std;
 
 int main() {
     Scan ltest;
-    ltest.srcfile = "/Users/TanGreen/ClionProjects/Woo/Debug/main.txt";
+    string s;
+    cout << "input the code file: ";
+    cin >> s;
+    ltest.srcfile = s;
     ltest.outfile = "/Users/TanGreen/ClionProjects/Woo/Debug/out.txt";
     ltest.getToken();
     Parse ptest(ltest.list_token, "/Users/TanGreen/ClionProjects/Woo/Debug/asm.txt",
